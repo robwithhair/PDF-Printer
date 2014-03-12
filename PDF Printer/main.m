aecc81e1d02306a680ef563698de87a2f2777020
Rob Harrison <robwithhair@gmail.com>
2014-03-12 01:44:21 +0000

    Introduced some arguments

--- PDF Printer/main.m
+++ PDF Printer/main.m
@@ -12,7 +12,11 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
+        NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
+        NSLog(@"boolArg   = %d", [args boolForKey:@"boolArg"]);
+        NSLog(@"intArg    = %ld", (long)[args integerForKey:@"intArg"]);
+        NSLog(@"floatArg  = %f", [args floatForKey:@"floatArg"]);
+        NSLog(@"stringArg = %@", [args stringForKey:@"stringArg"]);
         // insert code here...
         NSLog(@"My first objective C program");
         
