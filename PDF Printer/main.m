c79aaa0c9eec64c218b3903c38d4ef6964a00dda
Rob Harrison <robwithhair@gmail.com>
2014-03-12 15:44:20 +0000

    Added Quartz framework

--- PDF Printer/main.m
+++ PDF Printer/main.m
@@ -7,18 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Quartz/Quartz.h>
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
-        NSLog(@"boolArg   = %d", [args boolForKey:@"boolArg"]);
-        NSLog(@"intArg    = %ld", (long)[args integerForKey:@"intArg"]);
-        NSLog(@"floatArg  = %f", [args floatForKey:@"floatArg"]);
-        NSLog(@"stringArg = %@", [args stringForKey:@"stringArg"]);
+        //NSLog(@"boolArg   = %d", [args boolForKey:@"boolArg"]);
+        //NSLog(@"intArg    = %ld", (long)[args integerForKey:@"intArg"]);
+        //NSLog(@"floatArg  = %f", [args floatForKey:@"floatArg"]);
+        NSLog(@"PDF Filepath = %@", [args stringForKey:@"file"]);
         // insert code here...
         NSLog(@"My first objective C program");
+        
         
     }
     return 0;
