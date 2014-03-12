ec9e311d9c92635477831c477ec9dbed37bf0035
Rob Harrison <robwithhair@gmail.com>
2014-03-12 01:27:17 +0000

    First Commit

--- PDF Printer/main.m
+++ PDF Printer/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"My first objective C program");
         
     }
     return 0;
